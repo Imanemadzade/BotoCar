be2321ba7a2d4e481396f79043133ce493c772f4
Iman-emadzade <imanemadzade0@gmail.com>
2026-01-02 16:47:16 +0330

    feat : SearchBar & filter price added to project

--- pages/cars/index.js
+++ pages/cars/index.js
@@ -1,10 +1,12 @@
 import Categories from "../../components/layout/module/Categories";
+import SearchBar from "../../components/layout/module/SearchBar";
 import CarsPage from "../../components/layout/temlates/CarsPage";
 import carsData from "../../data/carsData";
 
 function Details() {
   return (
     <div>
+      <SearchBar />
       <Categories />
       <CarsPage data={carsData} />
     </div>
